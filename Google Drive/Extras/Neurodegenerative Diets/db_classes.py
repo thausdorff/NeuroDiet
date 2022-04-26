b43eaef601fea22b2099cbcbf32ee99d722f0cc4
Tuvia Hausdorff <thausdorff@gmail.com>
2022-04-26 21:44:04 +0300

    created db

--- Google Drive/Extras/Neurodegenerative Diets/db_classes.py
+++ Google Drive/Extras/Neurodegenerative Diets/db_classes.py
@@ -5,7 +5,7 @@ from sqlalchemy.types import ARRAY
 
 Base = declarative_base()
 # global engine, Session, session
-path = "sqlite://///C:/Users/thaus/Google Drive/Extras/Neurodegenerative Diets/data.db"
+path = "sqlite:///C:/Users/thaus/Google Drive/Extras/Neurodegenerative Diets/neuroDiet.db"
 engine = create_engine(path)
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -25,37 +25,43 @@ class Diets(Base):
     __tablename__ = "diets"
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    item = relationship("Item", back_populates='diets')
+    # item = relationship("Item", back_populates='diets')
 
 class Item(Base):
     __tablename__ = "item"
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    good_for_diet = relationship(ARRAY(Integer), ForeignKey=("diets.id"))
-    bad_for_diet = relationship(ARRAY(Integer), ForeignKey=("diets.id"))
+    good_for_diet_ids = Column(Integer, ForeignKey("diets.id"))
+    bad_for_diet_ids = Column(Integer, ForeignKey("diets.id"))
+
+    good_for_diet = relationship("Diets", foreign_keys=[good_for_diet_ids])
+    bad_for_diet = relationship("Diets", foreign_keys=[bad_for_diet_ids])
 
 class Diseases(Base):
     __tablename__ = "diseases"
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    good = relationship(ARRAY(Integer), ForeignKey=("diets.id"))
-    bad = relationship(ARRAY(Integer), ForeignKey=("diets.id"))
-    user = relationship("User", back_populates='diseases')
+    good = Column(Integer, ForeignKey("diets.id"))
+    bad = Column(Integer, ForeignKey("diets.id"))
+    user = relationship("User")
 
 
 def create_diets():
     diets = ["Ketogenic", "Mediterranean", "Vegan", "MIND", "Volumetrics", "Paleo"]
-    for diet in diets:
-        session.add(Diets(name=diet))
+    for d in diets:
+        diet = Diets(name=d)
+        session.add(diet)
     session.commit()
 
 def add_test_food():
-    foods = [("Red Meat", [], ["Mediterranean", "MIND", "Vegan"]), ("Fish", ["Mediterranean"], ["Vegan"])]
+    foods = ["Red Meat, "Mediterranean", "MIND", "Vegan"]), ("Fish", ["Mediterranean"], ["Vegan"])]
     for food in foods:
         item = Item(name= food[0], good_for_diet= food[1], bad_for_diet= food[2])
         session.add(item)
     session.commit()
 
+Base.metadata.create_all(engine)
+session.commit()
 create_diets()
 add_test_food()
 session.close()
