a2eefff06c635a9934b2c6653dce53a725d97942
Tuvia Hausdorff <thausdorff@gmail.com>
2022-04-27 15:01:31 +0300

    added nutrtional value class

--- Google Drive/Extras/Neurodegenerative Diets/db_classes.py
+++ Google Drive/Extras/Neurodegenerative Diets/db_classes.py
@@ -25,21 +25,36 @@ class Diets(Base):
     __tablename__ = "diets"
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    rating = relationship("Rating", back_populates= "diet") 
+    rating = relationship("DietFood", back_populates= "diet") 
 
 class Item(Base):
     __tablename__ = "item"
     id = Column(Integer, primary_key=True)
+    fdc_id = Column(Integer)
     name = Column(String)
-    rating = relationship("Rating", back_populates= "item")
-    # good_for_diet_ids = Column(Integer, ForeignKey("diets.id"))
-    # bad_for_diet_ids = Column(Integer, ForeignKey("diets.id"))
+    rating = relationship("DietFood", back_populates= "item")
+    nutritional_value = relationship("FoodNutrition", back_populates="item")
 
-    # good_for_diet = relationship("Diets", foreign_keys=[good_for_diet_ids])
-    # bad_for_diet = relationship("Diets", foreign_keys=[bad_for_diet_ids])
 
-class Rating(Base):
-    __tablename__ = "ratings"
+class Nutrition(Base):
+    __tablename__ = "nutrition"
+    id = Column(Integer, primary_key=True, autoincrement=False)
+    name = Column(String)
+    unit = Column(String)
+    nutritional_value = relationship("FoodNutrition", back_populates ="nutrition")
+
+
+class FoodNutrition(Base):
+    __tablename__ = "food_nutrition"
+    id = Column(Integer, primary_key=True)
+    fdc_id = Column(Integer, ForeignKey('item.fdc_id'))
+    item = relationship("Item", back_populates= "nutritional_value")
+    nutrition_id = Column(Integer, ForeignKey('nutrition.id'))
+    nutrition = relationship("Nutrition", back_populates= "nutritional_value")
+
+
+class DietFood(Base):
+    __tablename__ = "diet_foods"
     id = Column(Integer, primary_key = True)
     diet_id = Column(Integer, ForeignKey("diets.id"))
     diet = relationship("Diets", back_populates= "rating")
@@ -56,29 +71,29 @@ class Diseases(Base):
     user = relationship("User")
 
 
-def create_diets():
-    diets = ["Ketogenic", "Mediterranean", "Vegan", "MIND", "Volumetrics", "Paleo"]
-    for d in diets:
-        diet = Diets(name=d)
-        session.add(diet)
-    session.commit()
+# def create_diets():
+#     diets = ["Ketogenic", "Mediterranean", "Vegan", "MIND", "Volumetrics", "Paleo"]
+#     for d in diets:
+#         diet = Diets(name=d)
+#         session.add(diet)
+#     session.commit()
 
-def add_test_food():
-    foods = ["Red Meat", "Fish", "Tomato", "Green Apple", "Chicken", "Nuts"]
-    for food in foods:
-        session.add(Item(name=food))
-    session.commit()
+# def add_test_food():
+#     foods = ["Red Meat", "Fish", "Tomato", "Green Apple", "Chicken", "Nuts"]
+#     for food in foods:
+#         session.add(Item(name=food))
+#     session.commit()
 
-def add_rating():
-    foods = ["Red Meat", "Fish", "Tomato", "Green Apple", "Chicken", "Nuts"]
-    for food in foods:
-        item = session.query(Item).filter(Item.name == food).first()
-        diet = session.query(Diets).filter(Diets.name == diet).first()
-        session.add(Rating(diet_id = diet.id, item_id = item.id, good = False))
-    session.commit()
+# def add_rating():
+#     foods = ["Red Meat", "Fish", "Tomato", "Green Apple", "Chicken", "Nuts"]
+#     for food in foods:
+#         item = session.query(Item).filter(Item.name == food).first()
+#         diet = session.query(Diets).filter(Diets.name == diet).first()
+#         session.add(Rating(diet_id = diet.id, item_id = item.id, good = False))
+#     session.commit()
 
 Base.metadata.create_all(engine)
 session.commit()
-create_diets()
-add_test_food()
+# create_diets()
+# add_test_food()
 session.close()
